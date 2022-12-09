e182dc14baa7385a344c51872841d7a70469fb89
smat3847 <103894191+smat3847@users.noreply.github.com>
2022-12-09 11:47:09 +0530

    Update script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,15 @@
-
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("core").style.marginLeft = "250px";
+  document.getElementById("naad").style.marginLeft = "250px";
+  document.getElementById("naad1").style.marginLeft = "250px";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("core").style.marginLeft = "0";
+  document.getElementById("naad").style.marginLeft = "0";
+ 
 }
+
+
